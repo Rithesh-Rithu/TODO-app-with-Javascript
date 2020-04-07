cfe695f8741f0d92b1e89831cbf11f34df4d0733
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-04-07 11:57:16 +0530

    little changes in todo.js

--- script/todo.js
+++ script/todo.js
@@ -1,24 +1,9 @@
-// var h =document.createElement('h1')
-// var myValue = document.createTextNode('hello world!!')
-// h.appendChild(myValue)
-// document.querySelector('h1').appendChild(h)
-
-
 var ul = document.getElementById('list');
 var li;
-
-
 var addButton = document.getElementById('add');
 addButton.addEventListener('click',addItem);
-
 var removeButton = document.getElementById('remove');
 removeButton.addEventListener('click',removeItem); 
-
-
-
-
-
-
 
 function addItem(){
    var input = document.getElementById('input');
@@ -26,38 +11,28 @@ function addItem(){
    var textnode = document.createTextNode(item)
    ul=document.getElementById('list');
 
-if (item === ''){
-    return false;
-}else{
+   if (item === ''){
+     return false;
+     }
+   else{
     li = document.createElement('li');
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.setAttribute('id','check');
-var label = document.createElement('label');
-label.setAttribute('for','item');
-
-ul.appendChild(label);
-li.appendChild(checkbox);
-label.appendChild(textnode);
-li.appendChild(label);
-ul.insertBefore(li,ul.childNodes[0]);
-
-setTimeout(() => {
+    var label = document.createElement('label');
+    label.setAttribute('for','item');
+    ul.appendChild(label);
+    li.appendChild(checkbox);
+    label.appendChild(textnode);
+    li.appendChild(label);
+    ul.insertBefore(li,ul.childNodes[0]);
+    setTimeout(() => {
     li.className='visual'; 
-}, 3);
-input.value='';
-
-
-
+    }, 3);
+    input.value='';
+    }
 }
-
-
-
-
-}
-
 function removeItem(){
-    // console.log('delete button clicked!!1');
     li = ul.children
     console.log(li);
     for (let index = 0; index < li.length; index++) {
@@ -66,6 +41,5 @@ function removeItem(){
         while(li[index] && li[index].children[0].checked){
             ul.removeChild(li[index])
         }
-        
     }
 }
